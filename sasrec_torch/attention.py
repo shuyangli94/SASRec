f05a45dbd193f65d58fd89113bf05963c2471bab
shuyangli94 <shuyangli94@users.noreply.github.com>
2019-05-31 11:49:55 -0700

    checkpoint

--- sasrec_torch/attention.py
+++ sasrec_torch/attention.py
@@ -1,46 +1,66 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import numpy as np
 
-class SelfAttention(nn.Module):
+class CausalSelfAttention(nn.Module):
     """
     Implements Scaled Dot Product attention (Vaswani et al. 2017)
     https://papers.nips.cc/paper/7181-attention-is-all-you-need.pdf
 
+    Includes causality, etc. from (Kang & McAuley 2018):
+    https://cseweb.ucsd.edu/~jmcauley/pdfs/icdm18.pdf
+
     Attn(Q, K, V) = softmax(QK^T / sqrt(d)) V
     """
-    def __init__(self, hidden_size):
+    def __init__(self, hidden_size, causality=True, dropout=0.2):
         super().__init__()
 
         # Hidden size & Scaling
         self.hidden_size = hidden_size
         self.scaling = np.sqrt(self.hidden_size)
+        
+        # Dropout
+        self.dropout = nn.Dropout(p=dropout)
 
         # Projections into the hidden space
         self.key_layer = nn.Linear(hidden_size, hidden_size, bias=False)
         self.query_layer = nn.Linear(hidden_size, hidden_size, bias=False)
         self.value_layer = nn.Linear(hidden_size, hidden_size, bias=False)
 
-    def forward(self, embedding, mask=None, copy=None, ratings=None):
+        # Causality (Future Blinding)
+        self.causality = causality
+
+    def forward(self, embedding, mask=None, copy=None, verbose=False):
         # Project the embedding into the key, query, value spaces
         # Embedding: [B; D]
         # Projected: [B; D]
         key = self.key_layer(embedding)
         query = self.query_layer(embedding)
         value = self.value_layer(embedding)
+        if verbose:
+            print('Key size: {}'.format(key.size()))
+            print('Query size: {}'.format(query.size()))
+            print('Value size: {}'.format(value.size()))
 
         # Calculate scores
         scores = torch.matmul(query, torch.t(key)) / self.scaling
+        if verbose:
+            print('Scores size: {}'.format(scores.size()))
 
         # Mask out invalid positions
         # The mask marks valid positions so we invert it using `mask & 0`
         if mask is not None:
+            if verbose:
+                print('Mask size: {}'.format(mask.size()))
             scores.data.masked_fill_(
                 mask.unsqueeze(1) == 0, float('-inf')
             )
 
         # Turn scores to probabilities
         alphas = F.softmax(scores, dim=-1)
+        if verbose:
+            print('Alphas size: {}'.format(alphas.size()))
 
         # Copy over existing probability distribution to modify attention weights
         if copy is not None:
@@ -50,8 +70,13 @@ class SelfAttention(nn.Module):
             # Normalize to perform a convex combination
             alphas = F.normalize(alphas, p=1, dim=-1)
 
+        # Dropout
+        alphas = self.dropout(alphas)
+
         # The context vector is the weighted sum of the values.
         context = torch.bmm(alphas, value)
+        if verbose:
+            print('Context size: {}'.format(context.size()))
 
         # context shape: [B, 1, 2D], alphas shape: [B, 1, M]
         return context, alphas
--- sasrec_torch/model.py
+++ sasrec_torch/model.py
@@ -3,20 +3,84 @@ import torch.nn as nn
 
 import numpy as np
 
+from .attention import CausalSelfAttention
+from . import count_parameters
+
 class SASRec(nn.Module):
 
-    def __init__(self, hidden_size, max_seq_len, n_items):
+    def __init__(self, hidden_size, max_seq_len, n_items, dropout=0.2):
         super().__init__()
 
-        # Parameters
+        '''
+        Parameters
+        '''
         self.hidden_size = hidden_size      # d
         self.max_seq_len = max_seq_len      # n
         self.n_items = n_items              # |I|
+        self.dropout = dropout
 
-        # Positional embedding
-        # [n; d]
-        self.positional_embedding = nn.Embedding(
+        '''
+        Positional embedding
+        P ~ n x d
+
+        *This doesn't need lookup - it's always added to the sequence
+        '''
+        self.positional_embedding = torch.FloatTensor(
             self.max_seq_len, hidden_size
         )
 
-        # 
+        '''
+        Item embedding
+        M ~ |I| x d
+        '''
+        self.item_embedding = nn.Embedding(
+            self.n_items, self.hidden_size
+        )
+
+        '''
+        Self-Attention module
+        '''
+        self.attention_module = CausalSelfAttention(self.hidden_size)
+
+        '''
+        Feed-forward network
+        '''
+        self.ffn = nn.Sequential(
+            nn.Linear(self.hidden_size, self.hidden_size, bias=True),
+            nn.Dropout(p=dropout),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size, self.hidden_size, bias=True)
+        )
+
+        print('Created "{}" model with {:,} parameters:'.format(
+            self.__class__.__name__, count_parameters(self)
+        ))
+        print('Hidden size {:,}'.format(self.hidden_size))
+        print('Max sequence length {:,}'.format(self.max_seq_len))
+        print('Item vocabulary {:,}'.format(self.n_items))
+        print('[Dropout: {:,.3f}]'.format(self.dropout))
+    
+    def forward(self, ixn_seq, verbose=False):
+        # E^hat = [M + P]
+        input_embedding = self.positional_embedding + self.item_embedding(ixn_seq)
+        if verbose:
+            print('Input embedding size: {}'.format(input_embedding.size()))
+
+        # Attention
+        context, attn_scores = self.attention_module(input_embedding, verbose=verbose)
+
+        # Feed-forward aggregator
+        output = self.ffn(context)
+
+        return output
+
+'''
+python -m sasrec_torch.model
+'''
+if __name__ == "__main__":
+    hidden_size = 32
+    max_seq_len = 50
+    n_items = 170000
+    dropout = 0.2
+
+    model = SASRec(hidden_size, max_seq_len, n_items, dropout)
